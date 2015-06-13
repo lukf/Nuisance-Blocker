92492f07946e0b2537204dda9d1fb24379afda33
Lukas Funk <nxtlukas@me.com>
2015-06-13 14:29:11 +0200

    Initial working examples
    3 blockers for letterboxd and tumblr, using CSS selectors.

---
+++ ActionRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ActionRequestHandler.swift
+//  Nuisance Blocklist
+//
+//  Created by Lukas Funk on 13/06/15.
+//  Copyright © 2015 Aldebaran. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequestWithExtensionContext(context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOfURL: NSBundle.mainBundle().URLForResource("blockerList", withExtension: "json"))!
+    
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+    
+        context.completeRequestReturningItems([item], completionHandler: nil);
+    }
+    
+}
